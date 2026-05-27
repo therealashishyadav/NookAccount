515da612070bc0a55389de11043b9b057ae3588d
Ashish Yadav <ashishyadav01111998@gmail.com>
2026-05-27 11:55:47 +0530

    new add 1

--- src/main/java/com/Account/Config/WebConfig.java
+++ src/main/java/com/Account/Config/WebConfig.java
@@ -1,22 +1,25 @@
-//package com.Account.Config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//	@Override
-//	public void addCorsMappings(CorsRegistry registry) {
-//	    registry.addMapping("/**")
-//	            .allowedOrigins(
-//	                "http://localhost:4200",
-//	                "https://nookly-frontend-hslj.vercel.app",   // ADD THIS
-//	                "https://apigateway-x0ku.onrender.com"        // ADD THIS
-//	            )
-//	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-//	            .allowedHeaders("*")
-//	            .allowCredentials(true);
-//	}
-//}
+package com.Account.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	    registry.addMapping("/**")
+	            .allowedOrigins(
+	                "http://localhost:4200",
+	                "https://nookly-frontend-hslj.vercel.app",
+	                "https://cribup.vercel.app",
+	                "https://apigateway-x0ku.onrender.com",
+	                "https://nookly-git-main-therealashishyadav1.vercel.app",
+	                "https://nookly-4kojk0tjf-therealashishyadav1.vercel.app"
+	            )
+	            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+	            .allowedHeaders("*")
+	            .allowCredentials(true);
+	}
+}
