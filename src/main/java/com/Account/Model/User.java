10021e3dd9d59f05fb8c662c42813313d6c1ee43
Ashish Yadav <ashishyadav01111998@gmail.com>
2026-08-13 10:15:15 +0530

    new code push

--- src/main/java/com/Account/Model/User.java
+++ src/main/java/com/Account/Model/User.java
@@ -101,16 +101,16 @@ public class User implements UserDetails {
 		this.password = password;
 	}
 
-//	@Override
-//	public Collection<? extends GrantedAuthority> getAuthorities() {
-//
-//		return List.of(new SimpleGrantedAuthority(role.name()));
-//	}
-
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-	    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+
+		return List.of(new SimpleGrantedAuthority(role.name()));
 	}
+
+//	@Override
+//	public Collection<? extends GrantedAuthority> getAuthorities() {
+//	    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+//	}
 	
 	@Override
 	public String getUsername() {
